45d3594103154f29b9fdeb7864c7c8568fa8752f
wkyukim-biz <wkyukim-biz@crossent.com>
2018-06-02 17:58:03 +0900

    google vision api 테스트 예제 프로젝트 최초 등록

---
+++ Proof-of-Work/GoogleVisionApi_Example/src/main/java/com/example/demo/AbountGoogleVisionApiApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbountGoogleVisionApiApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(AbountGoogleVisionApiApplication.class, args);
+		testApi();
+	}
+
+	private static void testApi() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
